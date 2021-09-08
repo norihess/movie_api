28f40e03c394e1de2db4a0d83cc38db35648edae
norihess <Norihess19@gmail.com>
2021-09-08 13:05:31 -0600

    added server

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ http.createServer((request, response) => {
   });
 
 }).listen(8080);
-console.log('My test server is running on Port 8080.');
+console.log('My test server is running on Port 8080');
