f0b2148348a888855d2e488c2edda24eb0fdea62
norihess <Norihess19@gmail.com>
2021-09-22 10:16:38 -0600

    adding more code to pull genres, director and movie title

--- index.js
+++ index.js
@@ -4,24 +4,7 @@ let http = require('http'),
   addr = 'http://localhost:8080/',
   uuid = require('uuid');
 
-//   http.createServer((request, response) => {
-//   let requestURL = url.parse(request.url, true);
-//   if ( requestURL.pathname == '/documentation.html') {
-//     response.writeHead(200, {'Content-Type': 'text/plain'});
-//     response.end('Documentation on the movieclub API.\n');
-//   } else {
-//     response.writeHead(200, {'Content-Type': 'text/plain'});
-//     response.end('Welcome to the club!\n');
-//   }
-//
-// }).listen(8080);
-
-// console.log('My first Node test server is running on Port 8080.');
-//
-// }).listen(8080);
-// console.log('My test server is running on Port 8080.');
-
-//movies
+//require()
 let express = require('express'),
   app = express(),
   morgan = require('morgan'),
@@ -32,72 +15,88 @@ let express = require('express'),
   console.log(req.url);
   next();
 };
-
+//downloaded packages
 app.use(morgan('common'));
 app.use(myLogger);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+//movies
 let movies = [
   {
-    title: 'The Conjuring',
-    director: {
-      name: 'Michael Chaves',
+    Title: 'The Conjuring',
+    Director: {
+      Name: 'Michael Chaves',
       DateOfBirth: 'October 25, 1989',
       DateOfDeath: 'Alive at age 32.'
     },
-    genre:'Horror, Thriller',
-    description:'The Conjuring is a 2013 supernatural horror film inspired by the true-life story of the Perron family, who claimed they "lived among the dead" in the 1970s as spirits both friendly and sinister inhabited their Rhode Island farmhouse.'
+    Genre:{
+      Type:'Horror, Thriller',
+      Description:'The Conjuring is a 2013 supernatural horror film inspired by the true-life story of the Perron family, who claimed they "lived among the dead" in the 1970s as spirits both friendly and sinister inhabited their Rhode Island farmhouse.'
+    }
   },
 
   {
-    title: 'The Addams Family',
-    director: {
-      name: 'Barry Sonnenfeld',
+    Title: 'The Addams Family',
+    Director: {
+      Name: 'Barry Sonnenfeld',
       DateOfBirth: 'April 1, 1953',
       DateOfDeath: 'Alive at age 68.'
     },
-    genre: 'Horror, Dark Comedy',
-    description:'Addams Family characters include Gomez, Morticia, Uncle Fester, Lurch, Grandmama, Wednesday and Pugsley. The Addamses are a satirical inversion of the ideal American family; an eccentric, wealthy clan who delight in the macabre and are unaware that people find them bizarre or frightening.'
+    Genre: {
+      Type: 'Horror, Dark Comedy',
+      Description:'Addams Family characters include Gomez, Morticia, Uncle Fester, Lurch, Grandmama, Wednesday and Pugsley. The Addamses are a satirical inversion of the ideal American family; an eccentric, wealthy clan who delight in the macabre and are unaware that people find them bizarre or frightening.'
+      }
   },
 
   {
-    title: 'Nightmare Before Christmas',
-    director:{
-      name: 'Henry Selick',
+    Title: 'Nightmare Before Christmas',
+    Director:{
+      Name: 'Henry Selick',
       DateOfBirth: 'November 30, 1952',
       DateOfDeath: 'Alive at age 68.'
     },
-    genre: 'Musical, Animation',
-    description:'It tells the story of Jack Skellington, the King of "Halloween Town" who stumbles upon "Christmas Town" and becomes obsessed with celebrating the holiday. Danny Elfman wrote the songs and score, and provided the singing voice of Jack.'
+    Genre: {
+      Type: 'Musical, Animation',
+      Description:'It tells the story of Jack Skellington, the King of "Halloween Town" who stumbles upon "Christmas Town" and becomes obsessed with celebrating the holiday. Danny Elfman wrote the songs and score, and provided the singing voice of Jack.'
+    }
   },
 
   {
-    title: 'The Heat',
-    director:{
-      name:'Paul Feig',
+    Title: 'The Heat',
+    Director:{
+      Name:'Paul Feig',
       DateOfBirth: 'September 17, 1962',
       DateOfDeath: 'Alive at age 59.'
     },
-    genre: 'Comedy, Action',
-    description:'FBI Special Agent Sarah Ashburn (Sandra Bullock) is a methodical investigator with a long-standing reputation for excellence -- and arrogance. In contrast, foul-mouthed, hot-tempered detective Shannon Mullins (Melissa McCarthy) goes with her gut instincts and street smarts to remove criminals from the streets of Boston. Sparks fly when these polar opposites have to work together to capture a drug lord, but in the process, they become the last thing anyone expected -- buddies.'
+    Genre: {
+      Type: 'Comedy, Action',
+      Description:'FBI Special Agent Sarah Ashburn (Sandra Bullock) is a methodical investigator with a long-standing reputation for excellence -- and arrogance. In contrast, foul-mouthed, hot-tempered detective Shannon Mullins (Melissa McCarthy) goes with her gut instincts and street smarts to remove criminals from the streets of Boston. Sparks fly when these polar opposites have to work together to capture a drug lord, but in the process, they become the last thing anyone expected -- buddies.'
+    }
   },
 
   {
-    title: 'Jungle Cruise',
-    director: {
-      name:'Jaume Collet-Serra',
+    Title: 'Jungle Cruise',
+    Director: {
+      Name:'Jaume Collet-Serra',
       DateOfBirth: 'March 23, 1974',
       DateOfDeath: 'Alive at age 47.'
     },
-    genre:'Comedy, Action',
-    description:'Dr. Lily Houghton enlists the aid of wisecracking skipper Frank Wolff to take her down the Amazon in his ramshackle boat. Together, they search for an ancient tree that holds the power to heal -- a discovery that will change the future of medicine.'
-  },
+    Genre: {
+      Type: 'Comedy, Action',
+      Description:'Dr. Lily Houghton enlists the aid of wisecracking skipper Frank Wolff to take her down the Amazon in his ramshackle boat. Together, they search for an ancient tree that holds the power to heal -- a discovery that will change the future of medicine.'
+    }
+  }
 ];
 
-// GET requests
-// app.use(bodyParser.json());
+//functions
+// function movies(title, director, genre, description){
+//   movie.title = title;
+//   movie.director= director;
+//   movie.genre = genre;
+//   movie. description = description;
+// }
 
 // GET requests
 app.get('/', (req, res) => {
@@ -108,80 +107,78 @@ app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-//gets ALL movies
+//GETS ALL movies
 app.get('/movies', (req, res) => {
-  Movies.find()
-  .then((movies) => {
-    res.status(201).json(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error: ', err);
-  });
+  res.json(movies);
 });
-
-// Get data about a certain movie
-app.get('/movies/:Title', (req, res) => {
-  Movies.findOne({Title: req.params.Title})
-  .then((movie) => {
+//GETS movie by title
+app.get('/movie/titles' (req, res) =>{
+  movies.findOne({title: req.params.title})
+  .then((movie) =>{
     res.json(movie);
-  })
-  .catch((err) => {
+    return movies.title === req.params.title
+  }).catch ((err) => {
     console.error(err);
-    res.status(500).send('Error: ', err);
+    res.status(500).send('There is no movies of this title');
   });
 });
 
-// // //list ALL directors
-// app.get('/director', (req,res)=>{
-//   let directors = movies.directors(movie => ['directors']);
-//   res.json(directors);
+//GETS ALL directors
+// app.get('/directors', (req,res)=>{
+//   let directors = moveis.map(movies => ['director']);
+//   // let directors = movies.map(movies => ['directors']);
+//   res.json(director);
 // });
-// // Gets the director info
-// app.get('/director/:Info', (req, res) => {
-//   directors.fineOne({
-//     name: req.params.name,
-//     DateOfBirth: req.params.DateOfBirth,
-//     DateOfDeath: req.params.DateOfDeath
-//   })
-//   .then((director) => {
-//     res.json(director);
-//     });
-//   });
-// gets list of All directors
-  app.get('/directors', (req, res) => {
-    Directors.find()
+app.get("/directors", (req, res) => {
+  Directors.find().populate('Movie')
     .then((directors) => {
       res.status(201).json(directors);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ', err);
+      res.status(500).send("Error: " + err);
     });
-  });
-
-  // Get data about a certain movie
-  app.get('/movies/:Title', (req, res) => {
-    Movies.findOne({Title: req.params.Title})
-    .then((movie) => {
-      res.json(movie);
+});
+// Gets the director name
+app.get("/directors/:Name", (req, res) => {
+  Directors.findOne({ Name: req.params.Name })
+    .then((director) => {
+      res.json(director);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ', err);
+      res.status(500).send("Error: " + err);
     });
-  });
-// //list of genre
-app.get('/genre', (req, res)=>{
-  let genre = movies.map(movies => ['genre']);
-  // let uniqueGenre = [...new Set(genre)];
-  res.json(genre);
 });
-// //get genres
-app.get('/movies/:genre', (req, res) => {
-  g
-  res.json(movies.find((genre) =>
-    { return movies.uniqueGenre === req.params.uniqueGenre }));
+
+//GETS ALL genres and descriptions
+// app.get('/genre', (req, res)=>{
+//   let genre = movies.map(movies => ['genre']);
+//   // let uniqueGenre = [...new Set(genre)];
+//   res.json(genre);
+// });
+get all genres
+app.get('/genres', (req, res) => {
+  Genres.find()
+  .then((genre) => {
+    res.status(201).json(genre);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+})
+
+//get genre by name
+app.get("/genres/:Name", (req, res) => {
+  Genres.findOne({ Name: req.params.Name })
+    .then((genre) => {
+      res.json(genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.get('/description', (req,res)=>{
