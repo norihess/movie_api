33e50121a87b3101c4aa11102b3ebf642f274f4b
norihess <Norihess19@gmail.com>
2021-09-22 14:14:17 -0600

    added more seperate arrays

--- index.js
+++ index.js
@@ -89,7 +89,61 @@ let movies = [
     }
   }
 ];
+let directors = [
+  {
+    Name: 'Michael Chaves',
+    DateOfBirth: 'October 25, 1989',
+    DateOfDeath: 'Alive at age 32.'
+  },
+  {
+    Name: 'Barry Sonnenfeld',
+    DateOfBirth: 'April 1, 1953',
+    DateOfDeath: 'Alive at age 68.'
+  },
+  {
+    Name: 'Henry Selick',
+    DateOfBirth: 'November 30, 1952',
+    DateOfDeath: 'Alive at age 68.'
+  },
+  {
+    Name:'Paul Feig',
+    DateOfBirth: 'September 17, 1962',
+    DateOfDeath: 'Alive at age 59.'
+  },
+  {
+    Name:'Jaume Collet-Serra',
+    DateOfBirth: 'March 23, 1974',
+    DateOfDeath: 'Alive at age 47.'
+  }
+];
 
+let genres = [
+  //conjuring
+  {
+    Type:'Horror, Thriller',
+    Description:'The Conjuring is a 2013 supernatural horror film inspired by the true-life story of the Perron family, who claimed they "lived among the dead" in the 1970s as spirits both friendly and sinister inhabited their Rhode Island farmhouse.'
+  },
+  //Addams Fam
+  {
+    Type: 'Horror, Dark Comedy',
+    Description:'Addams Family characters include Gomez, Morticia, Uncle Fester, Lurch, Grandmama, Wednesday and Pugsley. The Addamses are a satirical inversion of the ideal American family; an eccentric, wealthy clan who delight in the macabre and are unaware that people find them bizarre or frightening.'
+  },
+  //Nightmare
+  {
+    Type: 'Musical, Animation',
+    Description:'It tells the story of Jack Skellington, the King of "Halloween Town" who stumbles upon "Christmas Town" and becomes obsessed with celebrating the holiday. Danny Elfman wrote the songs and score, and provided the singing voice of Jack.'
+  },
+  //the Heat
+  {
+    Type: 'Comedy, Action',
+    Description:'FBI Special Agent Sarah Ashburn (Sandra Bullock) is a methodical investigator with a long-standing reputation for excellence -- and arrogance. In contrast, foul-mouthed, hot-tempered detective Shannon Mullins (Melissa McCarthy) goes with her gut instincts and street smarts to remove criminals from the streets of Boston. Sparks fly when these polar opposites have to work together to capture a drug lord, but in the process, they become the last thing anyone expected -- buddies.'
+  },
+  //Jungle
+  {
+    Type: 'Comedy, Action',
+    Description:'Dr. Lily Houghton enlists the aid of wisecracking skipper Frank Wolff to take her down the Amazon in his ramshackle boat. Together, they search for an ancient tree that holds the power to heal -- a discovery that will change the future of medicine.'
+  }
+];
 // GET requests
 app.get('/', (req, res) => {
   res.send('Welcome to the club!');
@@ -106,8 +160,8 @@ app.get('/movies', (req, res) => {
 //GETS movie by title
 app.get('/movies/:Title', (req, res) =>{
   res.json(movies.find((movies) => {
-    return movies.title === req.params.title }))
-  .catch ((err) => {
+    return movies.title === req.params.title
+  })).catch ((err) => {
     console.error(err);
     res.status(500).send('There is no movies of this title');
   });
@@ -120,61 +174,52 @@ app.get('/movies/:Title', (req, res) =>{
 //   res.json(director);
 // });
 app.get('/directors', (req, res) => {
-  let Directors = movies.map (movies => ['directors']);
-  Directors.find().populate('Movie')
-    .then((directors) => {
-      res.status(201).json(directors);
-    })
-    .catch((err) => {
+  let directors = []; //define empty array
+  for (let i = 0; i < directors.length; i++) {
+    res.json(directors);
+  }
+    directors.catch((err) => {
       console.error(err);
       res.status(500).send("Error: " + err);
-    });
-});
-// Gets director by name
-app.get("/directors/:Name", (req, res) => {
-  Directors.findOne({ Name: req.params.Name })
-    .then((director) => {
-      res.json(director);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
 });
 
-//GETS ALL genres and descriptions
-// app.get('/genre', (req, res)=>{
-//   let genre = movies.map(movies => ['genre']);
-//   // let uniqueGenre = [...new Set(genre)];
-//   res.json(genre);
-// });
+// Gets director by name
+app.get("/directors/:Name", (req, res) => {
+  let directors = []; //define empty array
+  for (let i = 0; i < director.length; i++) {
+    let directorName = director[i].name
+    directors.push(directorName);
+    }
+    directors.catch((err) =>  {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+     });
+});
+
 // get all genres
 app.get('/genres', (req, res) => {
-  Genres.find()
-  .then((genres) => {
-    res.status(201).json(genres);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
+  let genres = []; //define empty array
+  for (let i = 0; i < genres.length; i++) {
+    res.json(genres);
+  }
+  genres.catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    })
   });
-})
 
 //get genre by name
 app.get("/genres/:Type", (req, res) => {
-  Genres.findOne({ Type: req.params.Type })
-    .then((genre) => {
-      res.json(genre);
-    })
-    .catch((err) => {
+  let genres = []; //define empty array
+  for (let i = 0; i < genres.length; i++) {
+    let genreType = genres[i].type + genres[i].description
+    director.push(genreType);
+  }
+    genres.catch((err) => {
       console.error(err);
       res.status(500).send("Error: " + err);
     });
-});
-
-app.get('/description', (req,res)=>{
-  let descriptions = movies.map (movies => ['description']);
-  res.json(description);
 });
 
 //POST
