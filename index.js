a892f22eb6974223464c133bb6ec9035ee19c727
norihess <Norihess19@gmail.com>
2021-09-25 10:11:07 -0600

    added and removed cover images

--- index.js
+++ index.js
@@ -33,9 +33,10 @@ let movies = [
       DateOfDeath: 'Alive at age 32.'
     },
     Genre:{
-      Type:'Horror, Thriller',
+      Type:'Horror',
       Description:'The Conjuring is a 2013 supernatural horror film inspired by the true-life story of the Perron family, who claimed they "lived among the dead" in the 1970s as spirits both friendly and sinister inhabited their Rhode Island farmhouse.'
-    }
+    },
+    movieID:'1'
   },
 
   {
@@ -47,9 +48,10 @@ let movies = [
       DateOfDeath: 'Alive at age 68.'
     },
     Genre: {
-      Type: 'Horror, Dark Comedy',
+      Type: 'Dark Comedy',
       Description:'Addams Family characters include Gomez, Morticia, Uncle Fester, Lurch, Grandmama, Wednesday and Pugsley. The Addamses are a satirical inversion of the ideal American family; an eccentric, wealthy clan who delight in the macabre and are unaware that people find them bizarre or frightening.'
-      }
+    },
+    movieID:'2'
   },
 
   {
@@ -61,9 +63,10 @@ let movies = [
       DateOfDeath: 'Alive at age 68.'
     },
     Genre: {
-      Type: 'Musical, Animation',
+      Type: 'Animation',
       Description:'It tells the story of Jack Skellington, the King of "Halloween Town" who stumbles upon "Christmas Town" and becomes obsessed with celebrating the holiday. Danny Elfman wrote the songs and score, and provided the singing voice of Jack.'
-    }
+    },
+    movieID:'3'
   },
 
   {
@@ -75,9 +78,10 @@ let movies = [
       DateOfDeath: 'Alive at age 59.'
     },
     Genre: {
-      Type: 'Comedy, Action',
+      Type: 'Action',
       Description:'FBI Special Agent Sarah Ashburn (Sandra Bullock) is a methodical investigator with a long-standing reputation for excellence -- and arrogance. In contrast, foul-mouthed, hot-tempered detective Shannon Mullins (Melissa McCarthy) goes with her gut instincts and street smarts to remove criminals from the streets of Boston. Sparks fly when these polar opposites have to work together to capture a drug lord, but in the process, they become the last thing anyone expected -- buddies.'
-    }
+    },
+    movieID:'4'
   },
 
   {
@@ -89,34 +93,40 @@ let movies = [
       DateOfDeath: 'Alive at age 47.'
     },
     Genre: {
-      Type: 'Comedy, Action',
+      Type: 'Action',
       Description:'Dr. Lily Houghton enlists the aid of wisecracking skipper Frank Wolff to take her down the Amazon in his ramshackle boat. Together, they search for an ancient tree that holds the power to heal -- a discovery that will change the future of medicine.'
-    }
+    },
+    movieID:'5'
   }
 ];
 //directors
 let directors = [
   {
+    directorID: '1',
     Name: 'Michael Chaves',
     DateOfBirth: 'October 25, 1989',
     DateOfDeath: 'Alive at age 32.'
   },
   {
+    directorID: '2',
     Name: 'Barry Sonnenfeld',
     DateOfBirth: 'April 1, 1953',
     DateOfDeath: 'Alive at age 68.'
   },
   {
+    directorID: '3',
     Name: 'Henry Selick',
     DateOfBirth: 'November 30, 1952',
     DateOfDeath: 'Alive at age 68.'
   },
   {
+    directorID: '4',
     Name:'Paul Feig',
     DateOfBirth: 'September 17, 1962',
     DateOfDeath: 'Alive at age 59.'
   },
   {
+    directorID: '5',
     Name:'Jaume Collet-Serra',
     DateOfBirth: 'March 23, 1974',
     DateOfDeath: 'Alive at age 47.'
@@ -126,30 +136,69 @@ let directors = [
 let genres = [
   //conjuring
   {
+    genreID: '1',
     Type:'Horror, Thriller',
     Description:'The Conjuring is a 2013 supernatural horror film inspired by the true-life story of the Perron family, who claimed they "lived among the dead" in the 1970s as spirits both friendly and sinister inhabited their Rhode Island farmhouse.'
   },
   //Addams Fam
   {
+    genreID: '2',
     Type: 'Horror, Dark Comedy',
     Description:'Addams Family characters include Gomez, Morticia, Uncle Fester, Lurch, Grandmama, Wednesday and Pugsley. The Addamses are a satirical inversion of the ideal American family; an eccentric, wealthy clan who delight in the macabre and are unaware that people find them bizarre or frightening.'
   },
   //Nightmare
   {
+    genreID: '3',
     Type: 'Musical, Animation',
     Description:'It tells the story of Jack Skellington, the King of "Halloween Town" who stumbles upon "Christmas Town" and becomes obsessed with celebrating the holiday. Danny Elfman wrote the songs and score, and provided the singing voice of Jack.'
   },
   //the Heat
   {
+    genreID: '4',
     Type: 'Comedy, Action',
     Description:'FBI Special Agent Sarah Ashburn (Sandra Bullock) is a methodical investigator with a long-standing reputation for excellence -- and arrogance. In contrast, foul-mouthed, hot-tempered detective Shannon Mullins (Melissa McCarthy) goes with her gut instincts and street smarts to remove criminals from the streets of Boston. Sparks fly when these polar opposites have to work together to capture a drug lord, but in the process, they become the last thing anyone expected -- buddies.'
   },
   //Jungle
   {
+    genreID: '5',
     Type: 'Comedy, Action',
     Description:'Dr. Lily Houghton enlists the aid of wisecracking skipper Frank Wolff to take her down the Amazon in his ramshackle boat. Together, they search for an ancient tree that holds the power to heal -- a discovery that will change the future of medicine.'
   }
 ];
+let Users =[
+  {
+    UserID: '1',
+    username: 'Brad Gig',
+    password: 'userPassWord1',
+    email: 'bradg@gmail.com',
+    birthday: 'October 4, 1987',
+    favoriteMovies: 'The Heat, Addams Family'
+  },
+  {
+    UserID: '2',
+    username: 'Jen Hemming',
+    password: 'userPassWord2',
+    email: 'jenhemming@gmail.com',
+    birthday: 'June 25, 1997',
+    favoriteMovies: 'Nightmare Before Christmas, Jungle Cruise'
+  },
+  {
+    UserID: '3',
+    username: 'Ashley Smith',
+    password: 'userPassWord3',
+    email: 'ashleys@gmail.com',
+    birthday: 'March 12, 1990',
+    favoriteMovies: 'The Conjuring, Addams Family'
+  },
+  {
+    UserID: '4',
+    username: 'James Morton',
+    password: 'userPassWord4',
+    email: 'jamesMort@gmail.com',
+    birthday: 'December 20, 1993',
+    favoriteMovies: 'Jungle Cruise, The Conjuring'
+  }
+]
 // GET requests
 app.get('/', (req, res) => {
   res.send('Welcome to the club!');
