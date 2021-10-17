f409725e38a44a7c80f37b807ec2ab4351d1ad19
norihess <Norihess19@gmail.com>
2021-10-17 17:57:53 -0600

    updated

--- index.js
+++ index.js
@@ -27,9 +27,8 @@ let Director = Models.Director;
 
 
 //connecting database with connction URI
-// mongoose.connect('mongodb://localhost:27017/myFlixDB',
-// { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/myFlixDB/',
+{ useNewUrlParser: true, useUnifiedTopology: true });
 
 //downloaded packages
 app.use(morgan('common'));
