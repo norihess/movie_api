e691049aea44fa63c752ad75e4b72c99ed8305e1
norihess <Norihess19@gmail.com>
2021-10-19 18:47:41 -0600

    changed back

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ app.put('/users/:Username', (req, res) => {
 // ADD a movie to a user's list of favorites
 app.post('/users/:Username/movies/:MovieID', (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, {
-     $push: [ FavoriteMovies: req.params.MovieID ]
+     $push: { FavoriteMovies: req.params.MovieID }
    },
    { new: true }, // This line makes sure that the updated document is returned
   (err, updatedUser) => {
