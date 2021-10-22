e0174aa32418c7c1016e8a7f4032c299ee1bcc68
norihess <Norihess19@gmail.com>
2021-10-22 10:00:49 -0600

    update

--- models.js
+++ models.js
@@ -23,7 +23,7 @@ let userSchema = mongoose.Schema ({
   Password: { type: String, required: true},
   Email: { type: String, required: true},
   Birdthday: Date,
-  FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: ['Movie']}]
+  FavoriteMovies: [{type: mongoose.Schema.Types.MovieID, ref: 'Movie'}]
 });
 
 //genre
