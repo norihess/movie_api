20f605dd35ee735848f84ee85693dcfc00afad81
norihess <Norihess19@gmail.com>
2021-10-27 11:06:36 -0600

    update

--- index.js
+++ index.js
@@ -200,7 +200,7 @@ app.put('/users/:Username', (req, res) => {
 });
 // ADD a movie to a user's list of favorites
 app.post('/users/:Username/Movies/:MovieID', (req, res) => {
-   Users.findByIdAndUpdate({Username: req.params.Username},
+   Users.findOneAndUpdate({Username: req.params.Username},
    {
      $push: {FavoriteMovies: req.params.MovieID}
    },
