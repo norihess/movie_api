e0ad1ecb2d53c6be4a4713c88def17ae668de6f6
norihess <Norihess19@gmail.com>
2021-11-03 13:08:45 -0600

    took out the auth.js code

--- passport.js
+++ passport.js
@@ -1,3 +1,4 @@
+//passport stragetgy
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
@@ -7,21 +8,23 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(new LocalStrategy({
+passport.use(new LocalStrategy(
+  {
   usernameField: 'Username',
   passwordField: 'Password'
-}, (username, password, callback) => {
-  console.log(username + '  ' + password);
-  Users.findOne({ Username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
-    }
+  },
+  (username, password, callback) => {
+    console.log(username + '  ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
 
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
-    }
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, {message: 'Incorrect username or password.'});
+        }
 
     console.log('finished');
     return callback(null, user);
@@ -40,41 +43,3 @@ passport.use(new JWTStrategy({
       return callback(error)
     });
 }));
-
-const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
-
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
-
-require('./passport'); // Your local passport file
-
-
-let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
-    subject: user.Username, // This is the username you’re encoding in the JWT
-    expiresIn: '7d', // This specifies that the token will expire in 7 days
-    algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
-  });
-}
-
-
-/* POST login. */
-module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
-        return res.status(400).json({
-          message: 'Something is not right',
-          user: user
-        });
-      }
-      req.login(user, { session: false }, (error) => {
-        if (error) {
-          res.send(error);
-        }
-        let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
-      });
-    })(req, res);
-  });
-}
