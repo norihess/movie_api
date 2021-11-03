0dee2acaf21777419f6f34ce44ab23c5294527bb
norihess <Norihess19@gmail.com>
2021-11-03 13:09:04 -0600

    updated

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 //authentication logic
 const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 const jwt = require('jsonwebtoken');
-// passport = require('passport');
+passport = require('passport');
 require('./passport'); // Your local passport file
 
 
