f4680a94b0ac595db641e790f40690c93f36a6e6
norihess <Norihess19@gmail.com>
2021-11-05 12:09:33 -0600

    updated

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(passport.initialize());
 app.use(express.json());
 
 //middleware
-// app.use(passport.initialize());
+app.use(passport.initialize());
 // app.use(passport.session());
 // app.use(passport.setAuthenticatedUser);
 
