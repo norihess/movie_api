3e9a0208c22c62c97b85a2a2718228e868884af1
norihess <Norihess19@gmail.com>
2022-06-22 13:36:58 -0600

    pushed

--- index.js
+++ index.js
@@ -36,19 +36,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //cors express
-let cors = require('cors');
-let allowedOrigins = ['http://localhost:4200', 'http://127.0.0.1:4200', 'http://localhost:8080', 'http://localhost:4200', 'http://testsite.com', 'http://localhost:1234', '`https://myflixangularclient.herokuapp.com/'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+const cors = require('cors');
+app.use(cors());
 
 //calling passport and authorization
 let auth = require('./auth')(app);
